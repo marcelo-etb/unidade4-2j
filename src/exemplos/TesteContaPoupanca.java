38bd32e8475f3e6ef01746aac86fbf41f1e5f2b1
marcelo-etb <mns.etb@gmail.com>
2024-05-17 00:59:21 -0300

    Inclusão das classes ContaPoupanca e TesteContaPoupanca

---
+++ src/exemplos/TesteContaPoupanca.java
@@ -0,0 +1,11 @@
+package exemplos;
+
+public class TesteContaPoupanca {
+    public static void main(String[] args) {
+        ContaPoupanca cp = new ContaPoupanca(111, "Lauane");
+        cp.depositar(1000);
+        System.out.println(cp);
+        cp.reajustar(1);
+        System.out.println(cp);
+    }
+}
